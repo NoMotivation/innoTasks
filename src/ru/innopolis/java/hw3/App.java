4d05dffffbf44c11f1016c23172d639ed29438f5
NoMotivation <127284536+NoMotivation@users.noreply.github.com>
2024-06-14 17:51:39 +0500

    Update hw3

--- src/ru/innopolis/java/hw3/App.java
+++ src/ru/innopolis/java/hw3/App.java
@@ -4,17 +4,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Television tv1 = new Television(TVModels.LG, 100.78);
+        Television tv1 = new Television(TVModels.LG, 100.7);
+        tv1.setTvPrice(89.5);
         double newPrice = tv1.priceWithDiscount(10);
         tv1.setDiscountPrice(newPrice);
+        tv1.setTvPrice(100);
+        System.out.println(tv1);
 
         Television tv2 = new Television(TVModels.valueOf("PANASONIC"), 90);
         tv2.setDiscountPrice(tv2.priceWithDiscount(25));
-        System.out.println(tv1);
         String tv2Info = tv2.toString();
         System.out.println(tv2Info);
-        System.out.println(tv2.getModelInfo());
-        System.out.println(tv2.getInfoPrice());
+
+        System.out.println(tv2.getTvModel());
+        System.out.println(tv2.getTvPrice());
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Выберите модель телевизора");
