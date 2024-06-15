33d725c71a9234ae6e515dbd28314097e6292b17
NoMotivation <127284536+NoMotivation@users.noreply.github.com>
2024-06-15 14:15:57 +0500

    homework03

--- src/ru/innopolis/java/homeworks/homework03/App.java
+++ src/ru/innopolis/java/homeworks/homework03/App.java
@@ -1,4 +1,4 @@
-package ru.innopolis.java.homeworks.homeworks03;
+package ru.innopolis.java.homeworks.homework03;
 
 import java.util.Scanner;
 
--- src/ru/innopolis/java/homeworks/homework03/TVModels.java
+++ src/ru/innopolis/java/homeworks/homework03/TVModels.java
@@ -1,4 +1,4 @@
-package ru.innopolis.java.homeworks.homeworks03;
+package ru.innopolis.java.homeworks.homework03;
 
 public enum TVModels {
     SAMSUNG ("Samsung"),
